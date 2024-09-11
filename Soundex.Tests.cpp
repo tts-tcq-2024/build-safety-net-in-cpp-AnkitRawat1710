83c9b8cda61af61d0696d94d276d6c866d95ee7d
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-09-12 00:29:59 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,11 +1,6 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(generateSoundex(""), "");
-}
-
-
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
